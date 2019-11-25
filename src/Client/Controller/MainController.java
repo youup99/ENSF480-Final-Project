818515e059c2b7809dd70dff82aed30270d95fae
Abhishek <57910394+Axial989@users.noreply.github.com>
2019-11-24 19:23:04 -0700

    Update MainController.java

--- src/Client/Controller/MainController.java
+++ src/Client/Controller/MainController.java
@@ -1,7 +1,50 @@
-public class MainController{
-    private MainView mainView;
+package Client.Controller;
 
-    public void addListenersToView(){
-        
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Client.Communication;
+import Client.View.MainView;
+
+public class MainController implements ActionListener
+{
+    private MainView mainView;
+    private Communication clientToServer; //TODO: Link depending on where Main for Client should go...
+    
+    private LoginController login;
+    private SearchController searchController;
+    
+    public MainController ()
+    {
+    	mainView = new MainView ();
+    	this.addListenersToView();
+    	mainView.setVisible(true);
     }
+
+    public void addListenersToView()
+    {
+        mainView.addLandlordListener(this);
+        mainView.addRenterListener(this);
+        mainView.addManagerListener(this);
+        mainView.addGuestListener(this);
+    }
+    
+    public static void main (String[] args)
+    {
+    	MainController controller = new MainController ();
+    }
+
+	@Override
+	public void actionPerformed (ActionEvent e) 
+	{
+		if (e.getActionCommand().equals("guest"))
+		{
+			
+		}
+		else
+		{
+			mainView.setVisible(false);
+			login = new LoginController (e.getActionCommand()); //Make mainview invisible and display login screen
+		}
+	}
 }
