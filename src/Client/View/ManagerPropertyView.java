f9f2ec89969fa56b0ff07d045043be68a2dda204
Minji Kim <minjikm19@gmail.com>
2019-11-25 21:35:02 -0700

    abstract

---
+++ src/Client/View/ManagerPropertyView.java
@@ -0,0 +1,41 @@
+package Client.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTable;
+
+import Client.Property;
+import Client.Controller.ManagerController;
+
+public class ManagerPropertyView extends PropertyView {
+	private ManagerController managerc;
+	
+	public ManagerPropertyView() {
+		editView.addSaveListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Property temp = editView.getSelectedProperty();
+		temp.setListingState(editView.getStatus());
+		managerc.updateStatus(temp);
+	}
+	
+	public void setManagerController(ManagerController mc) {
+		managerc = mc;
+	}
+
+	@Override
+	public void clickedAction(MouseEvent e) {
+		JTable target = (JTable) e.getSource();
+        int row = target.getSelectedRow();
+        Property sendData = propertyList.get(row);
+        editView = new EditPropertyView(sendData);
+	}
+
+
+
+
+}
---
+++ src/Client/View/RenterPropertyView.java
@@ -0,0 +1,25 @@
+package Client.View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTable;
+
+import Client.Property;
+
+public class RenterPropertyView extends PropertyView {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+	@Override
+	public void clickedAction(MouseEvent e) {
+		JTable target = (JTable) e.getSource();
+        int row = target.getSelectedRow();
+        Property sendData = propertyList.get(row);
+        propertyInfo = new PropertyInfoView(sendData);
+	}
+	
+}
